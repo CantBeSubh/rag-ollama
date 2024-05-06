2335052e0c83bc731403217ddc6445c7dd92075e
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-05-06 12:57:25 +0530

    Load Files

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,29 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain import hub
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from typing_extensions import TypedDict
+from typing import List
+from langchain.schema import Document
+from langgraph.graph import END, StateGraph
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.schema import Document
+from langchain_community.document_loaders import PyPDFLoader
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+local_llm = "llama3"
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+
 app = FastAPI()
 
 # Add middleware to enable CORS
@@ -23,9 +46,24 @@ async def upload_files(files: List[UploadFile] = File(...)):
         raise HTTPException(status_code=400, detail="No files were uploaded.")
 
     results = []
+    temp_dir = os.path.join(os.getcwd(), "temp")
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
     for file in files:
-        contents = await file.read()  # Read the contents of the uploaded file
-        # Process the PDF file here (you can use libraries like PyPDF2 or pdfplumber)
-        # For demonstration purposes, let's just return the filename and length of each file
+        contents = await file.read()
+
+        temp_file_path = os.path.join(temp_dir, file.filename)
+        with open(f"{temp_file_path}", "wb") as f:
+            f.write(contents)
+
+        try:
+            loader = PyPDFLoader(temp_file_path)
+            data = loader.load()
+            print(f"Data loaded for {file.filename}")
+        except Exception as e:
+            print(f"Failed to load {file.filename}: {str(e)}")
+        finally:
+            os.remove(temp_file_path)
+
         results.append({"filename": file.filename, "file_length": len(contents)})
     return results
